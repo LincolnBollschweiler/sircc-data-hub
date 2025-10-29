7f4907618262723f2e4c370a4aa9c016ff671975
Lincoln Bollschweiler <lincolnbollschweiler@gmail.com>
2025-10-28 18:45:28 -0600

    updates to client-services table view layout

--- src/components/ClientServices.tsx
+++ src/components/ClientServices.tsx
@@ -16,14 +16,13 @@ export default function ClientServices({
 		<div className="w-full overflow-x-auto">
 			<div className="min-w-[950px] max-w-[1920px] mx-auto">
 				{/* Table header */}
-				<div className="grid grid-cols-[40px,27%,32%,7%,8%,8%,18%] items-center gap-3 border-b border-gray-300 bg-gray-100 px-3 py-2 text-xs font-semibold text-gray-700 lg:text-base">
-					<div className="text-center"></div>
-					<div>Name</div>
-					<div>Description</div>
-					<div>Funds</div>
-					<div>Created</div>
-					<div>Updated</div>
-					<div className="text-right"></div>
+				<div className="grid grid-cols-[24%,31%,10%,9%,9%,17%] items-end border-b border-gray-300 bg-gray-100 py-2 text-xs font-semibold text-gray-700 lg:text-base">
+					<div className="text-center">Name</div>
+					<div className="text-center">Description</div>
+					<div className="text-center">Funds Required</div>
+					<div className="text-center">Created</div>
+					<div className="text-center">Updated</div>
+					<div></div>
 				</div>
 
 				{/* Table rows */}
--- src/components/SortableClientServicesList.tsx
+++ src/components/SortableClientServicesList.tsx
@@ -6,6 +6,7 @@ import { Button } from "./ui/button";
 import { FilePenLineIcon, Trash2Icon, GripVerticalIcon } from "lucide-react";
 import { ActionButton } from "./ActionButton";
 import { removeClientService, updateOrders } from "@/tableInteractions/actions";
+import { cn } from "@/lib/utils";
 
 export function SortableClientServicesList({
 	items,
@@ -24,21 +25,28 @@ export function SortableClientServicesList({
 	return (
 		<SortableList items={items} onOrderChange={updateOrders}>
 			{(sorted) =>
-				sorted.map((item) => (
+				sorted.map((item, index) => (
 					<SortableItem key={item.id} id={item.id}>
 						{({ attributes, listeners }) => (
-							<div className="grid grid-cols-[40px,27%,32%,7%,8%,8%,18%] items-center gap-3 px-3 py-2 border-b border-gray-200 bg-white">
-								{/* handle column */}
-								<div className="flex justify-center" {...attributes} {...listeners}>
-									<GripVerticalIcon className="text-muted-foreground cursor-grab w-5 h-5" />
-								</div>
-
+							<div
+								className={cn(
+									index % 2 === 0 ? "bg-white" : "bg-[gray-100]",
+									"w-full grid grid-cols-[24%,31%,10%,9%,9%,17%] items-start py-1 border-b border-gray-200 text-xs lg:text-base"
+								)}
+							>
 								{/* content columns */}
-								<div>{item.name}</div>
-								<div className="truncate">{item.description}</div>
-								<div>{item.dispersesFunds ? "Yes" : "No"}</div>
-								<div>{new Date(item.createdAt).toLocaleDateString("en-US", dateOptions)}</div>
-								<div>{new Date(item.updatedAt).toLocaleDateString("en-US", dateOptions)}</div>
+								<div className="flex items-center cursor-grab px-1 pt-2" {...attributes} {...listeners}>
+									<GripVerticalIcon className="text-muted-foreground w-5 h-5" />
+									<div>{item.name}</div>
+								</div>
+								<div className="px-1 pt-2">{item.description}</div>
+								<div className="text-center pt-2">{item.dispersesFunds ? "Yes" : "No"}</div>
+								<div className="text-center pt-2">
+									{new Date(item.createdAt).toLocaleDateString("en-US", dateOptions)}
+								</div>
+								<div className="text-center pt-2">
+									{new Date(item.updatedAt).toLocaleDateString("en-US", dateOptions)}
+								</div>
 
 								{/* actions column */}
 								<div className="flex justify-end gap-2">
